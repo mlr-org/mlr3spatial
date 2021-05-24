52d30fcae6da382e50670f24d6bb1a6e8531e43a
pat-s <patrick.schratz@gmail.com>
2021-05-24 08:33:58 +0000

    Deploy from Github Actions build 870754945 [ci skip]
    Build URL: https://github.com/mlr-org/mlr3spatial/actions/runs/870754945
    Commit: 2367765a7a916743d4f4e880dd0f30a32a9f48b5

--- docsearch.js
+++ docsearch.js
@@ -12,63 +12,63 @@ $(function() {
   $(document).ready(function() {
     // do keyword highlighting
     /* modified from https://jsfiddle.net/julmot/bL6bb5oo/ */
-    var mark = function() {
+      var mark = function() {
 
-      var referrer = document.URL ;
-      var paramKey = "q" ;
+        var referrer = document.URL ;
+        var paramKey = "q" ;
 
-      if (referrer.indexOf("?") !== -1) {
-        var qs = referrer.substr(referrer.indexOf('?') + 1);
-        var qs_noanchor = qs.split('#')[0];
-        var qsa = qs_noanchor.split('&');
-        var keyword = "";
+        if (referrer.indexOf("?") !== -1) {
+          var qs = referrer.substr(referrer.indexOf('?') + 1);
+          var qs_noanchor = qs.split('#')[0];
+          var qsa = qs_noanchor.split('&');
+          var keyword = "";
 
-        for (var i = 0; i < qsa.length; i++) {
-          var currentParam = qsa[i].split('=');
+          for (var i = 0; i < qsa.length; i++) {
+            var currentParam = qsa[i].split('=');
 
-          if (currentParam.length !== 2) {
-            continue;
-          }
-
-          if (currentParam[0] == paramKey) {
-            keyword = decodeURIComponent(currentParam[1].replace(/\+/g, "%20"));
-          }
-        }
-
-        if (keyword !== "") {
-          $(".contents").unmark({
-            done: function() {
-              $(".contents").mark(keyword);
+            if (currentParam.length !== 2) {
+              continue;
             }
-          });
-        }
-      }
-    };
 
-    mark();
+            if (currentParam[0] == paramKey) {
+              keyword = decodeURIComponent(currentParam[1].replace(/\+/g, "%20"));
+            }
+          }
+
+          if (keyword !== "") {
+            $(".contents").unmark({
+              done: function() {
+                $(".contents").mark(keyword);
+              }
+            });
+          }
+        }
+      };
+
+      mark();
   });
 });
 
 /* Search term highlighting ------------------------------*/
 
-function matchedWords(hit) {
-  var words = [];
+  function matchedWords(hit) {
+    var words = [];
 
-  var hierarchy = hit._highlightResult.hierarchy;
-  // loop to fetch from lvl0, lvl1, etc.
-  for (var idx in hierarchy) {
-    words = words.concat(hierarchy[idx].matchedWords);
+    var hierarchy = hit._highlightResult.hierarchy;
+    // loop to fetch from lvl0, lvl1, etc.
+    for (var idx in hierarchy) {
+      words = words.concat(hierarchy[idx].matchedWords);
+    }
+
+    var content = hit._highlightResult.content;
+    if (content) {
+      words = words.concat(content.matchedWords);
+    }
+
+    // return unique words
+    var words_uniq = [...new Set(words)];
+    return words_uniq;
   }
-
-  var content = hit._highlightResult.content;
-  if (content) {
-    words = words.concat(content.matchedWords);
-  }
-
-  // return unique words
-  var words_uniq = [...new Set(words)];
-  return words_uniq;
-}
 
 function updateHitURL(hit) {
 
