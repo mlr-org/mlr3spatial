7f7fdb25e2c3b6583ec094de1c5f67ab9c4251e8
Marc Becker <marcbecker@posteo.de>
2021-06-05 13:48:26 +0200

    rewrite

--- R/LearnerClassifSVMParallel.R
+++ R/LearnerClassifSVMParallel.R
@@ -1,10 +1,10 @@
-#' Parallel Prediction SVM Classification Learner
+#' @title Parallel Prediction SVM Classification Learner
 #'
 #' @description
-#' Proof of concept for parallel prediction intergrated into the [mlr3::Learner]
+#' Proof of concept for parallel prediction integrated into the [mlr3::Learner]
 #' class. The SVM classification learner is extended with the
 #' `predict_newdata_parallel` method which splits `newdata` into `n` parts and
-#' execudes the prediction in parallel. `n` is equal to the number of available
+#' executes the prediction in parallel. `n` is equal to the number of available
 #' cores.
 #'
 #' @note
@@ -18,12 +18,22 @@
 LearnerClassifSVMParallel = R6Class("LearnerClassifSVMParallel",
   inherit = LearnerClassifSVM,
   public = list(
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
     initialize = function() {
       super$initialize()
       self$properties = c(self$properties, "parallel_predict")
     },
 
+    #' @description
+    #' Predicts `newdata` in chunks.
+    #' 
+    #' @param newdata (`data.frame()`)\cr
+    #' New data to predict on
     predict_newdata_parallel = function(newdata) {
+      newdata = as.data.table(assert_data_frame(newdata, min.rows = 1L))
+
       n = future::availableCores()
       nr = nrow(newdata)
 
@@ -39,11 +49,11 @@ LearnerClassifSVMParallel = R6Class("LearnerClassifSVMParallel",
         future.packages = c("mlr3", "mlr3spatial"))
 
       res = do.call("c", res)
-      
       res$response
     }
   ))
 
+# wrapper for parallelization
 split_predict = function(newdata, self) {
   self$predict_newdata(newdata)
 }
--- R/PredictionRaster.R
+++ R/PredictionRaster.R
@@ -1,72 +1,89 @@
-#' Demo title
-#' @description demo descrip
-#' @importFrom raster blockSize raster writeStart getValues writeValues
+#' @title Prediction Raster
+#' 
+#' @description 
+#' This class handles raster predictions.
+#' 
 #' @export
 PredictionRaster = R6::R6Class("PredictionRaster",
   public = list(
+
+    #' @field stack ([terra::SpatRaster]).
     stack = NULL,
-    task = NULL,
-    chunksize = 100,
-    initialize = function(stack, task) {
-      self$stack = stack
-      self$task = task
+
+    #' @field chunksize (`integer(1)`)\cr
+    #' Size of raster chunks in megabyte.
+    chunksize = NULL,
+
+    #' @field reclassify_table ([data.table::data.table()])
+    reclassify_table = NULL,
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    #'
+    #' @param stack ([terra::SpatRaster])\cr
+    #' Raster stack of features.
+    #' @param chunksize (`integer(1)`)\cr
+    #' Size of raster chunks in megabyte.
+    initialize = function(stack, chunksize = 100) {
+      self$stack = assert_class(stack, "SpatRaster")
+      self$chunksize = assert_int(chunksize, lower = 1)
     },
+
+    #' @description 
+    #' Predicts raster with [mlr3::Learner].
+    #'
+    #' @param learner ([mlr3::Learner]).
+    #' @param filename (`character`)\cr
+    #' Output file path. 
     predict = function(learner, filename = tempfile(fileext = ".grd")) {
+      assert_learner(learner)
+      assert_names(learner$state$train_task$feature_names, identical.to = names(self$stack))
+      assert_path_for_output(filename)
       stack = self$stack
       chunksize = self$chunksize
       start_time = Sys.time()
 
-      # Calculate block size
-      tr = blockSize(stack, chunksize = chunksize * 1e+06, n = 4, minblocks = 1)
+      # calculate block size
+      tr = block_size(stack, chunksize)
 
-      # Initialize template raster
+      # initialize template raster
       template_raster = terra::rast(ext(stack), res = res(stack), crs = crs(stack))
       
-      # `target_raster` could replace `raster::blockSize`
-      target_raster = terra::writeStart(template_raster, filename=filename,  overwrite=TRUE)
-
-      terra::readStart(stack)
+      # open files for reading and writing
+      writeStart(template_raster, filename=filename, overwrite=TRUE)
+      readStart(stack)
 
       lg$info("Start raster prediction")
       lg$info("Prediction is executed in %i MB chunks", chunksize)
 
       for (i in 1:tr$n) {
-        new_data = as.data.table(as.data.frame(
-          getValues(stack, row = tr$row[i], nrows = tr$nrows[i])
-        ))
+        # read chunk of raster values
+        new_data = as.data.table(readValues(stack, row=tr$row[i], nrows=tr$nrows[i], dataframe = TRUE))
 
-        new_data = as.data.table(terra::readValues(stack, row=tr$row[i], nrows=tr$nrows[i], dataframe = TRUE))
-
-        if ("parallel_predict" %in% learner$properties) {
-          pred = learner$predict_newdata_parallel(new_data)
+        # predict chunk
+        pred = if ("parallel_predict" %in% learner$properties) {
+          learner$predict_newdata_parallel(new_data)
         } else {
-          pred = learner$predict_newdata(new_data)
+          learner$predict_newdata(new_data)
         }
-
-        pred = private$.classify(pred)
-
-        terra::writeValues(template_raster, pred, tr$row[i], tr$nrows[i])
+        
+        # reclassify predictions to integer values
+        if (learner$state$train_task$task_type == "classif") {
+          reclassify_table = data.table(task = task$class_names, raster = seq_along(task$class_names))
+          pred = reclassify_table$raster[match(pred, reclassify_table$task)]
+          self$reclassify_table = reclassify_table
+        }
+        
+        writeValues(template_raster, pred, tr$row[i], tr$nrows[i])
 
         lg$info("Chunk %i of %i finished", i, tr$n)
       }
-      writeStop(target_raster)
-      lg$info(
-        "Finished raster prediction in %i seconds",
+
+      writeStop(template_raster)
+      readStop(stack)
+      lg$info("Finished raster prediction in %i seconds",
         as.integer(difftime(start_time, Sys.time(), units = "secs") * (-1))
       )
-    }
-  ),
-  private = list(
-    .classify = function() {
-      stop("Abstract class")
-    }
-  ),
-  active = list(
-
-    #' @field raster_size
-    #' Returns raster size in memory
-    raster_size = function() {
-      c("Megabyte" = ncell(self$stack) * nlayers(self$stack) * 8 / 1e+06)
-    }
+    }    
   )
 )
