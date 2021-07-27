ca51b6efb8a61b8c4130d5d9d0928912e77dfaf0
Marc Becker <marcbecker@posteo.de>
2021-07-27 12:08:32 +0200

    seq_len and proc.time

--- R/predict_raster.R
+++ R/predict_raster.R
@@ -5,7 +5,7 @@ predict_raster = function(task, learner, chunksize = 100L, filename) {
   assert_int(chunksize)
   assert_path_for_output(filename)
   stack = task$backend$stack
-  start_time = Sys.time()
+  start_time = proc.time()[3]
 
   # calculate block size
   bs = block_size(stack, chunksize)
@@ -17,13 +17,12 @@ predict_raster = function(task, learner, chunksize = 100L, filename) {
   lg$info("Start raster prediction")
   lg$info("Prediction is executed in %i MB chunks", chunksize)
 
-  for (i in 1:bs$n) {
+  for (i in seq_len(bs$n)) {
     pred = learner$predict(task, row_ids = bs$row[i]:(bs$row[i] + bs$nrows[i] - 1))
     terra::writeValues(target_raster, pred$response, bs$row[i], bs$nrows[i])
     lg$info("Chunk %i of %i finished", i, bs$n)
   }
 
   writeStop(target_raster)
-  lg$info("Finished raster prediction in %i seconds",
-    as.integer(difftime(start_time, Sys.time(), units = "secs") * (-1)))
+  lg$info("Finished raster prediction in %i seconds", proc.time()[3] - start_time)
 }
