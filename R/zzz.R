9f26ad6f141135f3d284cd872999216a760c6c13
pat-s <patrick.schratz@gmail.com>
2021-08-10 21:28:27 +0200

    remove with_future() helper

--- R/zzz.R
+++ R/zzz.R
@@ -16,10 +16,3 @@
     lg$set_threshold("warn")
   }
 } # nocov end
-
-with_future = function(backend, expr, ...) {
-  requireNamespace("future")
-  oplan = force(future::plan(backend, ...))
-  on.exit(future::plan(oplan), add = TRUE)
-  force(expr)
-}
