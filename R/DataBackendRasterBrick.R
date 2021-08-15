30459abbdeacbee99c17ecc2a8c9c89d8afb7d76
pat-s <patrick.schratz@gmail.com>
2021-08-15 16:24:20 +0200

    save state

---
+++ R/DataBackendRasterBrick.R
@@ -0,0 +1,182 @@
+#' @title DataBackend for RasterBrick
+#'
+#' @description
+#' A [mlr3::DataBackend] for `RasterBrick` (package \CRANpkg{raster}).
+#'
+#' @param rows `integer()`\cr
+#'   Row indices. Row indices start with 1 in the upper left corner in the
+#'   raster, increase from left to right and then from top to bottom. The last
+#'   cell is in the bottom right corner and the row index equals the number of
+#'   cells in the raster.
+#' @param cols `character()`\cr
+#'   Column names.
+#'
+#' @section Read mode:
+#' * Block mode reads complete rows of the raster file and subsets the requested
+#'   cells. Faster than cell mode if we iterate the whole raster file.
+#'
+#' * Cell mode reads individual cells. Faster than block mode if only a few
+#'   cells are sampled.
+#'
+#' Block mode is activated if `$data(rows)` is called with a increasing integer
+#' sequence e.g. `200:300`.
+#' @importFrom raster readStart readStop rowColFromCell readValues head unique cats ncell intersect
+#' @export
+DataBackendRasterBrick = R6::R6Class("DataBackendRasterBrick",
+  inherit = mlr3::DataBackend, cloneable = FALSE,
+  public = list(
+
+    #' @description
+    #'
+    #' Creates a backend for a `RasterBrick`.
+    #'
+    #' @param data (`RasterBrick`)\cr
+    #'    A raster object.
+    #'
+    initialize = function(data) {
+      private$.data = assert_class(data, "RasterBrick")
+      self$data_formats = "data.table"
+    },
+
+    #' @description
+    #' Returns a slice of the data.
+    #' Calls [raster::rowColFromCell()] and [raster::readValues()] on the spatial
+    #' object and converts it to a [data.table::data.table()].
+    #'
+    #' The rows must be addressed as vector of primary key values, columns must
+    #' be referred to via column names. Queries for rows with no matching row id
+    #' and queries for columns with no matching column name are silently
+    #' ignored. Rows are guaranteed to be returned in the same order as `rows`,
+    #' columns may be returned in an arbitrary order. Duplicated row ids result
+    #' in duplicated rows, duplicated column names lead to an exception.
+    #' @param data_format (`character(1)`)\cr
+    #'  Desired data format, e.g. `"data.table"` or `"Matrix"`.
+    data = function(rows, cols, data_format = "data.table") {
+      stack = private$.data
+
+      if (isTRUE(all.equal(rows, rows[1]:rows[length(rows)]))) {
+        # block read
+        raster::readStart(stack)
+        on.exit(raster::readStop(stack))
+        # determine rows to read
+        cells = raster::rowColFromCell(stack, rows)
+        row = cells[1, 1]
+        nrows = cells[dim(cells)[1], 1] - cells[1, 1] + 1
+        res = as.data.table(raster::getValues(stack, row = row, nrows = nrows))
+        # subset cells and features
+        res = res[cells[1, 2]:(cells[1, 2] + length(rows) - 1), cols, with = FALSE]
+
+      } else {
+        # cell read
+        cells = raster::rowColFromCell(stack, rows)
+        res = data.table(apply(cells, 1, function(x) stack[x[1], x[2], cols]))
+        res = setNames(res, cols)
+      }
+      res
+    },
+
+    #' @description
+    #' Retrieve the first `n` rows.
+    #'
+    #' @param n (`integer(1)`)\cr
+    #'   Number of rows.
+    #'
+    #' @return [data.table::data.table()] of the first `n` rows.
+    head = function(n = 6L) {
+      # bogus
+      utils::head(as.data.table(raster::head(private$.data)), n)
+    },
+
+    #' @description
+    #' Returns a named list of vectors of distinct values for each column
+    #' specified. If `na_rm` is `TRUE`, missing values are removed from the
+    #' returned vectors of distinct values. Non-existing rows and columns are
+    #' silently ignored.
+    #' @param na_rm `logical(1)`\cr
+    #'   Whether to remove NAs or not.
+    #'
+    #' @return Named `list()` of distinct values.
+    distinct = function(rows, cols, na_rm = TRUE) {
+      assert_names(cols, type = "unique")
+      cols = raster::intersect(cols, self$colnames)
+      if (length(cols) == 0L) {
+        return(named_list(init = character()))
+      }
+
+      if (is.null(rows)) {
+        stack = raster::subset(private$.data, cols)
+        if (all(raster::is.factor(stack))) {
+          # fastest
+          # res = as.list(map_dtc(raster::cats(stack), function(layer) {
+          #   as.data.table(layer)[, 2]
+          # }))
+
+          res = raster::levels(stack)
+          res = stats::setNames(res, cols)
+        } else {
+          # fast
+          # bug: raster does not respect categorical raster layers
+          res = list(raster::unique(stack))
+          stats::setNames(res, cols)
+        }
+      } else {
+        # slow
+        data = self$data(rows, cols)
+        lapply(data, unique)
+      }
+    },
+
+    #' @description
+    #' Returns the number of missing values per column in the specified slice
+    #' of data. Non-existing rows and columns are silently ignored.
+    #'
+    #' @return Total of missing values per column (named `numeric()`).
+    missings = function(rows, cols) {
+      set_names(rep(0, self$ncol), self$colnames)
+    }
+  ),
+
+  active = list(
+    #' @field rownames (`integer()`)\cr
+    #' Returns vector of all distinct row identifiers, i.e. the contents of the
+    #' primary key column.
+    rownames = function(rhs) {
+      assert_ro_binding(rhs)
+      1:raster::ncell(private$.data)
+    },
+
+    #' @field colnames (`character()`)\cr
+    #' Returns vector of all column names.
+    colnames = function(rhs) {
+      assert_ro_binding(rhs)
+      names(private$.data)
+    },
+
+    #' @field nrow (`integer(1)`)\cr
+    #' Number of rows (observations).
+    nrow = function(rhs) {
+      assert_ro_binding(rhs)
+      raster::ncell(private$.data)
+    },
+
+    #' @field ncol (`integer(1)`)\cr
+    #' Number of columns (variables).
+    ncol = function(rhs) {
+      assert_ro_binding(rhs)
+      raster::nlyr(private$.data) + 1
+    },
+
+    #' @field stack (`integer(1)`)\cr
+    #' Returns RasterBrick.
+    stack = function(rhs) {
+      assert_ro_binding(rhs)
+      private$.data
+    }
+  ),
+
+  private = list(
+    .calculate_hash = function() {
+      mlr3misc::calculate_hash(self$compact_seq, private$.data)
+    }
+  )
+)
--- R/data.R
+++ R/data.R
@@ -33,9 +33,29 @@ demo_stack = function(size = 50, layers = 5) {
 
   dimension = floor(sqrt(size / layers * 1e+06 / 4))
   raster_features = replicate(layers - 1, demo_raster(dimension))
-  raster_response = rast(matrix(c(rep("FALSE", floor(dimension^2 / 2)),
+  raster_response = terra::rast(matrix(c(rep("FALSE", floor(dimension^2 / 2)),
     rep("TRUE", ceiling(dimension^2 / 2))), nrow = dimension))
   raster = rast(c(raster_features, list(raster_response)))
   names(raster) = c(paste0("x_", 1:(layers - 1)), "y")
   raster
+}
+
+demo_stack_raster = function(size = 50, layers = 5) {
+  assert_int(size, lower = 1)
+  assert_int(layers, lower = 1)
+
+  dimension = floor(sqrt(size / layers * 1e+06 / 4))
+  raster_features = replicate(layers - 1, demo_raster_raster(dimension))
+  raster_response = raster::raster(matrix(c(rep(FALSE, floor(dimension^2 / 2)),
+    rep(TRUE, ceiling(dimension^2 / 2))), nrow = dimension))
+  raster = raster::brick(c(raster_features, list(raster_response)))
+  names(raster) = c(paste0("x_", 1:(layers - 1)), "y")
+  raster
+}
+
+demo_raster_raster = function(dimension) {
+  assert_int(dimension, lower = 2)
+  data = matrix(c(stats::rnorm(floor(dimension^2 / 2), 0, 1),
+    stats::rnorm(ceiling(dimension^2 / 2), 1, 1)), nrow = dimension)
+  raster::raster(data)
 }
