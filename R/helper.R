6bab40b54736a15e82d0c9d007a20e00da633531
pat-s <patrick.schratz@gmail.com>
2021-09-04 00:21:18 +0200

    fix helper.R

--- R/helper.R
+++ R/helper.R
@@ -3,15 +3,22 @@
 write_raster = function(data) {
   # create temp file
   filename = tempfile(fileext = ".tif")
-  target_raster = terra::rast(ncols = terra::nrow(data), nrows = terra::nrow(data),
-    xmin = 0, xmax = terra::nrow(data), ymin = 0, ymax = terra::nrow(data))
+  # library(terra)
+  # browser()
+  # target_raster = terra::rast(ncols = nrow(data), nrows = nrow(data), xmin = 0,
+  # xmax = nrow(data), ymin = 0, ymax = nrow(data))
+  target_raster = terra::rast(data)
+  # browser()
   # calculate block size
   bs = block_size(target_raster, 100)
   # initialize target raster
   terra::writeStart(target_raster, filename = filename)
+  # browser()
   # write values in chunks
-  pmap(list(bs$row, bs$nrows), function(row, nrows) {
-    terra::writeValues(target_raster, data[row:(row + nrows - 1), 1:terra::nrow(data)], row, nrows)
+  pmap(list(bs$cells_seq, bs$cells_to_read), function(row, nrows) {
+    terra::writeValues(target_raster,
+      data[1:(terra::rowFromCell(target_raster, row) + terra::rowFromCell(target_raster, nrows) - 1),
+        1:terra::nrow(data)], terra::rowFromCell(target_raster, row), terra::rowFromCell(target_raster, nrows))
   })
   terra::writeStop(target_raster)
   terra::rast(filename)
@@ -29,7 +36,9 @@ write_raster = function(data) {
 #' @export
 demo_raster = function(dimension) {
   assert_int(dimension, lower = 2)
-  data = matrix(c(stats::rnorm(floor(dimension^2 / 2), 0, 1), stats::rnorm(ceiling(dimension^2 / 2), 1, 1)), nrow = dimension)
+  assert_int(dimension, lower = 2)
+  data = matrix(c(stats::rnorm(floor(dimension^2 / 2), 0, 1),
+    stats::rnorm(ceiling(dimension^2 / 2), 1, 1)), nrow = dimension)
   write_raster(data)
 }
 
@@ -70,18 +79,53 @@ demo_stack = function(size = 500, layers = 5) {
 #' @export
 block_size = function(raster, chunksize) {
   assert_class(raster, "SpatRaster")
+  # chunksize in bytes
   chunksize = assert_numeric(chunksize) * 1e+06
 
-  # one cell takes 8 byte memory
+  # row_size in bites; one cell takes 8 byte memory
   row_size = terra::ncol(raster) * terra::nlyr(raster) * 8
-  # number of rows in one block
-  nrow = chunksize / row_size
-  # start row indices
-  row = seq(1, nrow(raster), by = nrow)
+  # # browser()
+  # Hom many rows can be processed in one block?
+  nrow_block = chunksize / row_size
+  # How many cells are this?
+  ncells_block = nrow_block * terra::ncol(raster)
+  # split all cells by ncells_block
+  cells_seq = seq(1, terra::ncell(raster), by = ncells_block)
   # number of rows to read per block
-  nrows = rep(nrow, length(row))
-  nrows[length(nrows)] = terra::nrow(raster) - row[length(row)] + 1
+  cells_to_read = rep(ncells_block, length(cells_seq))
+  # browser()
 
+  cells_to_read[length(cells_to_read)] = terra::ncell(raster) - cells_seq[length(cells_seq)] + 1
 
-  return(list(row = row, nrows = nrows))
+  # # number of rows in one block
+  # nrow = chunksize / row_size
+  # # start row indices
+  # nrow_seq = seq(1, terra::nrow(raster), by = nrow)
+  # # number of rows to read per block
+  # rows_to_write = rep(nrow, length(nrow_seq))
+  # rows_to_write[length(rows_to_write)] = terra::nrow(raster) - nrow_seq[length(nrow_seq)] + 1
+
+  # browser()
+
+  # one cell is 8 bytes
+  # cellsize = 0.000008
+  # ncell = terra::ncell(raster)
+
+  # number of rows in one block
+  # # cell_size = terra::ncell(raster) * 8
+  # row_size = terra::ncol(raster) * terra::nlyr(raster) * 8
+  # # ncells_chunks = chunksize / cellsize
+  # # start row indices
+  # row = seq(1, terra::ncell(raster), by = row_size)
+  # # # number of rows to read per block
+  # # nrows = rep(nrow, length(row))
+  # # nrows[length(nrows)] = terra::nrow(raster) - row[length(row)] + 1
+
+  return(list(cells_seq = cells_seq, cells_to_read = cells_to_read # ,
+    # nrow_seq = nrow_seq, rows_to_write = rows_to_write
+  ))
 }
+
+stack_classif = demo_stack(1)
+value = data.table(ID = c(0, 1), y = c("negative", "positive"))
+terra::setCats(stack_classif, layer = "y", value = value)
