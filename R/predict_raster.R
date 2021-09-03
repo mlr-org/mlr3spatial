0b59c65153ab6d3e51d79b1ca5f7cf02629b63ee
pat-s <patrick.schratz@gmail.com>
2021-09-04 00:23:45 +0200

    clean predict_raster

--- R/predict_raster.R
+++ R/predict_raster.R
@@ -15,12 +15,16 @@ predict_raster = function(task, learner, chunksize = 100L, filename = tempfile(f
   terra::writeStart(target_raster, filename = filename)
 
   lg$info("Start raster prediction")
-  lg$info("Prediction is executed in %i MB chunks", chunksize)
+  lg$info("Prediction is executed in %i MB chunks, %i chunk(s) in total, %i values per chunk",
+    chunksize, length(bs$cells_seq), terra::ncell(task$backend$stack))
 
-  pmap(list(bs$row, bs$nrows, seq_along(bs$row)), function(row, nrows, n) {
-    pred = learner$predict(task, row_ids = row:(row + nrows - 1))
-    terra::writeValues(target_raster, pred$response, row, nrows)
-    lg$info("Chunk %i of %i finished", n, length(bs$row))
+  pmap(list(bs$cells_seq, bs$cells_to_read, seq_along(bs$cells_seq)), function(cells_seq, cells_to_read, n) {
+
+    pred = learner$predict(task, row_ids = cells_seq:((cells_seq + cells_to_read - 1)))
+    terra::writeValues(target_raster, pred$response,
+      terra::rowFromCell(task$backend$stack, cells_seq),
+      terra::rowFromCell(task$backend$stack, cells_to_read))
+    lg$info("Chunk %i of %i finished", n, length(bs$cells_seq))
   })
 
   terra::writeStop(target_raster)
