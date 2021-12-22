087869bf90464ec4a1595fb1c0f0b2ca691a46f1
pat-s <patrick.schratz@gmail.com>
2021-12-22 10:35:04 +0100

    clean

--- tic.R
+++ tic.R
@@ -4,7 +4,6 @@ if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
   get_stage("install") %>%
-    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate")) %>%
-    add_step(step_install_github("ropensci/tic@pkgdown-deploy"))
+    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown()
 }
